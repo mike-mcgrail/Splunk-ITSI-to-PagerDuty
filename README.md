77d4736d72eaa82f0c94affabdc4ad0b8ee88919
Mike <32044603+mike-mcgrail@users.noreply.github.com>
2019-11-22 10:37:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# pagerduty-itsi
+Splunk ITSI -> PagerDuty integration
+
+This is based on the original source code from Martin Stone at https://github.com/martindstone/pagerduty-itsi.
+
+This is a Notable Event Action that triggers a PagerDuty incident.  The following are updates from Martin Stone's original project:
+1. PagerDuty now has a standard Splunk integration.  The same integration can be used for Core Splunk and ITSI, but field mappings require a bit of changing from ITSI.
+2. PagerDuty now includes incidents and alerts.  By default, incidents are grouped and an incident number is not generated.  If alerts are disabled for a service, an incident number is still not a unique value so retrieving an incident number has been commented out in the code.
+3. Changed notable event action verbiage from "Trigger PagerDuty Incident" to "PagerDuty Incident Integration" to align with Splunk's Remedy Add-On verbiage
+4. Updated logging to follow ITSI standard logging
+5. HTTP call is routed through proxy in our environment, if not needed remove the proxy routing from the python script
+
+## Instructions for use:
+
+1. Create a Splunk Integration in PagerDuty as the default
+
+3. Copy pagerduty_itsi.py to SPLUNK_HOME/etc/apps/SA-ITOA/bin
+
+4. Copy pagerduty_itsi.html to SPLUNK_HOME/etc/apps/SA-ITOA/default/data/ui/alerts
+
+5. Edit SPLUNK_HOME/etc/apps/SA-ITOA/local/alert_actions.conf and add the following text at the bottom:
+
+	```
+	[pagerduty_itsi]
+	is_custom = 1
+	param.integration_url = <your integration URL from Step 1>
+	param.token = <your API token from Step 2>
+	label = Trigger PagerDuty Incident
+	description = Trigger an incident in PagerDuty
+	payload_format = json
+	```
+
+6. Edit SPLUNK_HOME/etc/apps/SA-ITOA/local/notable_event_actions.conf and add the following text at the bottom:
+
+	```
+	[pagerduty_itsi]
+	disabled = 0
+	execute_once_per_group = 0
+	```
+
+7. Restart Splunk: SPLUNK_HOME/bin/splunk restart
+
+You should now see a new item called "PagerDuty Incident Integration" in the Actions menu in Notable Events.
---
+++ pagerduty_itsi.py
@@ -0,0 +1,146 @@
+# Modified from original author Martin Stone at https://github.com/martindstone/pagerduty-itsi
+
+import sys
+import json
+import urllib2
+import time
+import re
+
+from fnmatch import fnmatch
+
+from splunk.clilib.bundle_paths import make_splunkhome_path
+
+sys.path.append(make_splunkhome_path(['etc', 'apps', 'SA-ITOA', 'lib']))
+
+from ITOA.itoa_config import get_supported_objects
+
+# Updated for ITSI 4.4 - logging was changed for Python 2/3 integration
+#from ITOA.setup_logging import setup_logging
+import itsi_path
+from itsi_py23 import _
+from ITOA.setup_logging import logger
+
+# updated for ITSI 4.0 - event actions were moved to EventGroup
+#from itsi.event_management.sdk.eventing import Event
+from itsi.event_management.sdk.grouping import EventGroup
+from itsi.event_management.sdk.custom_event_action_base import CustomEventActionBase
+
+
+def send_notification(payload):
+    settings = payload.get('configuration')
+    logger.info("Sending incident with settings %s" % settings)
+
+    url = settings.get('integration_url_override')
+    token = settings.get('token')
+
+    if not url:
+        url = settings.get('integration_url')
+
+    # check if only the integration key was given
+    if len(url) == 32:
+        url = 'https://events.pagerduty.com/integration/' + url + "/enqueue"
+
+   # Call functin to update payload for PagerDuty
+    try:
+        pd_body = modify_payload(payload)
+    except:
+        logger.error("Error modifying payload for PagerDuty")
+        pd_body = payload
+
+    body = json.dumps(pd_body,ensure_ascii=False).encode('utf8')
+    logger.info('Calling url="%s" with body=%s' % (url, body))
+    req = urllib2.Request(url, body, {"Content-Type": "application/json"})
+
+    #Route external traffic through proxy
+    proxy = "<proxy>.<domain>:<port>"
+    proxies = {"https":"https://%s" % proxy}
+    proxy_support = urllib2.ProxyHandler(proxies)
+    opener = urllib2.build_opener(proxy_support)
+    urllib2.install_opener(opener)
+
+    try:
+        res = urllib2.urlopen(req)
+        body = res.read()
+        logger.info("PagerDuty server responded with HTTP status=%d" % res.code)
+        if res.code < 200 or res.code > 299:
+            return False
+    except urllib2.HTTPError, e:
+        logger.error("Error sending message: %s (%s)" % (e, str(dir(e))))
+        return False
+
+    #event_id = payload['result']['event_id']
+    #Note: For ITSI, use itsi_group_id instead of event_id
+    event_id = payload['result']['itsi_group_id']
+    session_key = payload['session_key']
+    time.sleep(5) # wait 5 seconds for an incident to be created
+    
+    try:
+        event = EventGroup(session_key)
+        event.create_comment(event_id, "Successfully sent PagerDuty alert to %s" % url)
+    except Exception as e:
+        logger.error("Unknown error of type %s: %s" % (type(e).__name__, e))
+        event.create_comment(event_id, "Unable to create PD incident for event ID %s: Unknown error: %s" % (event_id, e))
+
+    return True
+
+
+def modify_payload(payload):     #Function to modify JSON payload before sending to PagerDuty, allowing PagerDuty Splunk integration to be reused for ITSI
+    body_string = json.dumps(payload)
+    body_load = json.loads(body_string)
+    pd_body = {}
+
+    #PagerDuty maps alert title to Core Splunk search_name; ITSI episode title is result.title
+    result_title = body_load['result']['title']
+    pd_body['search_name'] = result_title
+
+    #Update link to event (requires import re)
+    #FROM Core Splunk https://<server>/app/SA-ITOA/@go?sid=<sid_here>
+    #TO ITSI https://<server>/app/itsi/itsi_event_management?earliest=-24h&episodeid=<itsi_group_id>&tabid=impact
+    results_link = body_load['results_link']
+    result_itsi_group_id = body_load['result']['itsi_group_id']
+    new_link = 'itsi/itsi_event_management?earliest=-24h&episodeid=' + result_itsi_group_id + '&tabid=impact'
+    updated_results_link = re.sub(r'SA-ITOA.*', new_link, results_link)
+    pd_body['results_link'] = updated_results_link
+
+    #Update other fields
+    pd_body['configuration'] = body_load['configuration']    #configuration includes token, integration_url, integration_url_override
+    pd_body['app'] = body_load['app']
+
+    pd_body['result'] = {}
+    pd_body['result']['correlation_search_name'] = body_load['result']['search_name']
+    pd_body['result']['_raw'] = body_load['result']['_raw']
+    pd_body['result']['itsi_group_description'] = body_load['result']['itsi_group_description']
+
+    #Include optional fields if they exist
+    pd_body['result']['_time'] = body_load['result']['_time']
+    if 'actual_time' in body_load['result']:
+        pd_body['result']['actual_time'] = body_load['result']['actual_time']
+
+    pd_body['result']['service_name'] = '' 
+    if 'service_name' in body_load['result']:
+        pd_body['result']['service_name'] = body_load['result']['service_name']
+
+    if 'drilldown_title' in body_load['result']:
+        drilldown_title = body_load['result']['drilldown_title']
+        pd_body['result'][drilldown_title] = ''
+        if 'drilldown_uri' in body_load['result']:
+            pd_body['result'][drilldown_title] = body_load['result']['drilldown_uri']
+    elif 'drilldown_uri' in body_load['result']:
+        pd_body['result']['drilldown_uri'] = body_load['result']['drilldown_uri']
+ 
+    return pd_body
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "--execute":
+        payloadStr = sys.stdin.read()
+        payload = json.loads(payloadStr)
+        success = send_notification(payload)
+        if not success:
+            logger.error("Failed trying to send incident alert")
+            sys.exit(2)
+        else:
+            logger.info("Incident alert notification successfully sent")
+    else:
+        logger.error("FATAL Unsupported execution mode (expected --execute flag)")
+        sys.exit(1)
